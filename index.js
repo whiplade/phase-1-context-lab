57ab9cef0c0cbea46ca99fcbeb2f57fad529c628
whiplade <nmawira13@gmail.com>
2023-07-03 23:52:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,23 +1,76 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+function createEmployeeRecord(testEmployee) {
+  return {
+    firstName: testEmployee[0],
+    familyName: testEmployee[1],
+    title: testEmployee[2],
+    payPerHour: testEmployee[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
 }
 
+// Function to create multiple employee records
+function createEmployeeRecords(employeeTests) {
+  return employeeTests.map(createEmployeeRecord);
+}
+
+// Function to add a timeIn event to an employee's record
+function createTimeInEvent(dateTemplate) {
+  const [date, hour] = dateTemplate.split(" ");
+  this.timeInEvents.push({ type: "TimeIn", date: date, hour: parseInt(hour) });
+  return this;
+}
+
+// Function to add a timeOut event to an employee's record
+function createTimeOutEvent(dateTemplate) {
+  const [date, hour] = dateTemplate.split(" ");
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    date: date,
+    hour: parseInt(hour),
+  });
+  return this;
+}
+
+// Function to calculate the hours worked on a specific date for an employee
+function hoursWorkedOnDate(dateTemplate) {
+  const timeInEvent = this.timeInEvents.find(
+    (event) => event.date === dateTemplate
+  );
+  const timeOutEvent = this.timeOutEvents.find(
+    (event) => event.date === dateTemplate
+  );
+  const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+  return hoursWorked;
+}
+
+// Function to calculate the wages earned on a specific date for an employee
+function wagesEarnedOnDate(dateTemplate) {
+  const hoursWorked = hoursWorkedOnDate.call(this, dateTemplate);
+  const wagesEarned = hoursWorked * this.payPerHour;
+  return wagesEarned;
+}
+
+// Function to calculate the total wages earned by an employee for all dates
+function allWagesFor() {
+  const allDates = this.timeInEvents.map((event) => event.date);
+  const totalWages = allDates.reduce(
+    (total, date) => total + wagesEarnedOnDate.call(this, date),
+    0
+  );
+  return totalWages;
+}
+
+// Function to find an employee by first name in a collection of employee records
+function findEmployeeByFirstName(collection, firstNameString) {
+  return collection.find((employee) => employee.firstName === firstNameString);
+}
+
+// Function to calculate the total payroll for an array of employee records
+function calculatePayroll(employeeRecords) {
+  const totalPayroll = employeeRecords.reduce(
+    (total, employee) => total + allWagesFor.call(employee),
+    0
+  );
+  return totalPayroll;
+}
